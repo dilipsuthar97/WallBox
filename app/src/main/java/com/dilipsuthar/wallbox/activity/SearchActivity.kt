4b8463a9e246efe76875ff98830e714e6208264f
dilipsuthar1997 <suthardilip696@gmail.com>
2019-09-01 16:44:30 +0530

    add speech-to-text for search query

--- app/src/main/java/com/dilipsuthar/wallbox/activity/SearchActivity.kt
+++ app/src/main/java/com/dilipsuthar/wallbox/activity/SearchActivity.kt
@@ -1,12 +1,16 @@
 package com.dilipsuthar.wallbox.activity
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 import androidx.viewpager.widget.ViewPager
 import butterknife.BindView
@@ -16,7 +20,9 @@ import com.dilipsuthar.wallbox.adapters.SectionPagerAdapter
 import com.dilipsuthar.wallbox.fragments.SearchCollectionFragment
 import com.dilipsuthar.wallbox.fragments.SearchPhotoFragment
 import com.dilipsuthar.wallbox.fragments.SearchUserFragment
+import com.dilipsuthar.wallbox.utils.PopupUtils
 import com.google.android.material.tabs.TabLayout
+import java.util.*
 
 class SearchActivity : BaseActivity() {
 
@@ -48,24 +54,7 @@ class SearchActivity : BaseActivity() {
         etSearch.requestFocus()
         etSearch.setOnEditorActionListener { textView, i, keyEvent ->
 
-            val query = etSearch.text.toString()
-            if (query.isNotEmpty()) {
-                Log.d(HomeActivity.TAG, query)
-                supportFragmentManager.beginTransaction().replace(R.id.search_photo_fragment, SearchPhotoFragment.newInstance(query))
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
-
-                supportFragmentManager.beginTransaction().replace(R.id.search_collection_fragment, SearchCollectionFragment.newInstance(query))
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
-
-                supportFragmentManager.beginTransaction().replace(R.id.search_user_fragment, SearchUserFragment.newInstance(query))
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
-            }
-
-            val view = this.currentFocus
-            if (view != null) {
-                val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(view.windowToken, 0)
-            }
+            searchQuery()
 
             true
         }
@@ -75,9 +64,53 @@ class SearchActivity : BaseActivity() {
             onBackPressed()
         }
 
+        // Button google Speech-to-text listener
         btnSpeechToTxt.setOnClickListener {
+            /** This will open google's speech-to-text activity */
+            val speechIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH)
+            speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
 
+            if (speechIntent.resolveActivity(packageManager) != null)
+                startActivityForResult(speechIntent, 101)
+            else PopupUtils.showToast(this, "Your device don't support speech input", Toast.LENGTH_SHORT)
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            101 -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    etSearch.setText(result?.get(0) ?: "")
+                    searchQuery()
+                }
+            }
+        }
+    }
+
+    /** @method search query and change all search fragments */
+    private fun searchQuery() {
+        val query = etSearch.text.toString()
+        if (query.isNotEmpty()) {
+            Log.d(HomeActivity.TAG, query)
+            supportFragmentManager.beginTransaction().replace(R.id.search_photo_fragment, SearchPhotoFragment.newInstance(query))
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
+
+            supportFragmentManager.beginTransaction().replace(R.id.search_collection_fragment, SearchCollectionFragment.newInstance(query))
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
+
+            supportFragmentManager.beginTransaction().replace(R.id.search_user_fragment, SearchUserFragment.newInstance(query))
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
+        }
+
+        val view = this.currentFocus
+        if (view != null) {
+            val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
